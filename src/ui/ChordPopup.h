46466fc0c6fd86bc65df6b3d752e5f742a355459
masseyis <masseyis@me.com>
2025-12-05 18:51:19 +0000

    Add chord type and scale degree definitions

--- src/ui/ChordPopup.h
+++ src/ui/ChordPopup.h
@@ -28,4 +28,51 @@ struct ScaleDegree {
     bool isMinor;           // true for ii, iii, vi, vii°
 };
 
+// All available chord types
+static const std::vector<ChordType> kChordTypes = {
+    // Triads (3 notes)
+    {"maj",     {0, 4, 7}},
+    {"min",     {0, 3, 7}},
+    {"dim",     {0, 3, 6}},
+    {"aug",     {0, 4, 8}},
+    // Sevenths (4 notes)
+    {"7",       {0, 4, 7, 10}},
+    {"maj7",    {0, 4, 7, 11}},
+    {"min7",    {0, 3, 7, 10}},
+    {"dim7",    {0, 3, 6, 9}},
+    {"m7b5",    {0, 3, 6, 10}},
+    // Extended (5+ notes)
+    {"9",       {0, 4, 7, 10, 14}},
+    {"maj9",    {0, 4, 7, 11, 14}},
+    {"min9",    {0, 3, 7, 10, 14}},
+    {"11",      {0, 4, 7, 10, 14, 17}},
+    {"13",      {0, 4, 7, 10, 14, 17, 21}},
+    // Sus/Add (3-4 notes)
+    {"sus2",    {0, 2, 7}},
+    {"sus4",    {0, 5, 7}},
+    {"add9",    {0, 4, 7, 14}},
+};
+
+// Major scale degrees (for scale-locked chord selection)
+static const std::vector<ScaleDegree> kMajorScaleDegrees = {
+    {"I",    0,  false},
+    {"ii",   2,  true},
+    {"iii",  4,  true},
+    {"IV",   5,  false},
+    {"V",    7,  false},
+    {"vi",   9,  true},
+    {"vii",  11, true},
+};
+
+// Minor scale degrees
+static const std::vector<ScaleDegree> kMinorScaleDegrees = {
+    {"i",    0,  true},
+    {"ii",   2,  true},
+    {"III",  3,  false},
+    {"iv",   5,  true},
+    {"v",    7,  true},
+    {"VI",   8,  false},
+    {"VII",  10, false},
+};
+
 } // namespace ui
